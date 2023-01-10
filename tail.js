e3a1bda7a9a871ac44ab85b3f449bd0f65194cd3
cchand-vanc <cmchandler@live.co.uk>
2023-01-09 18:03:22 -0800

    eslint

---
+++ tail.js
@@ -0,0 +1,19 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`💚 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`💔 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+let tail = function(array) {
+  let shifted = [];
+  for (let i = 1; i < array.length; i++) {
+    shifted.push(array[i]);
+  }
+  return shifted;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
