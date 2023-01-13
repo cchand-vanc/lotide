2d948a1ce884f2eeb45970dbc0fa1edb3c1e06dd
cchand-vanc <cmchandler@live.co.uk>
2023-01-12 19:28:01 -0800

    Created function to return an array only up to a certain point (that was defined by the callback

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+let assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray) === true) {
+    return console.log(`💚 Assertion passed: ${firstArray} === ${secondArray}`);
+  } else {
+    return console.log(`💔 Assertion failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+let eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, -7, 2, 1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+assertArraysEqual([1, 2, 5], results1);
+assertArraysEqual(["I've", "been", "to", "Hollywood"], results2);
