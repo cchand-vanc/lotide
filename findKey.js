e3014b9af446e5215afebc1a81abb63eba4114bb
cchand-vanc <cmchandler@live.co.uk>
2023-01-12 19:58:58 -0800

    Finding a key through using a callback to search through an object for the corresponding value

---
+++ findKey.js
@@ -0,0 +1,34 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`💚 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`💔 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+let findKey = (object, callback) => {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")
